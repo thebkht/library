b15adabaa0a0d3c95e55d808bdc20650a44dce14
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-04 01:33:04 +0900

    Update next.config.mjs to use domains instead of remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "bkhtdev.com",
-        port: "",
-        path: "/**",
-      },
-    ],
+    domains: ["bkhtdev.com"],
+    loader: "default",
   },
 };
 
