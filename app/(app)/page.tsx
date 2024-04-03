41b1e977c67f94cb86cb5912891fd19966ecac4c
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-04 01:03:53 +0900

    Add next/link and page-header components

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,9 +1,41 @@
-import Image from "next/image";
+import Image from "next/image"
+import Link from "next/link"
 
-export default function Home() {
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "@/components/page-header"
+import { buttonVariants } from "@/components/ui/button"
+
+export default function IndexPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-    </main>
-  );
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading>Build your component library</PageHeaderHeading>
+        <PageHeaderDescription>
+          Beautifully designed components that you can copy and paste into your
+          apps. Accessible. Customizable. Open Source.
+        </PageHeaderDescription>
+        <PageActions>
+          <Link href="/docs" className={cn(buttonVariants())}>
+            Get Started
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <Icons.gitHub className="mr-2 h-4 w-4" />
+            GitHub
+          </Link>
+        </PageActions>
+      </PageHeader>
+    </div>
+  )
 }
