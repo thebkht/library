7b3c9dfda030d44d889511dbd2a8e1e199c0173a
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-04-03 14:39:41 +0900

    Init site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "bkht/library",
+  url: "https://ui.bkhtdev.com",
+  ogImage: "https://ui.bkhtdev.com/og.jpg",
+  description:
+    "A personal library of books, articles, and other resources that I've found useful.",
+  links: {
+    twitter: "https://twitter.com/yusupovbg",
+    github: "https://github.com/yusupovbg/library",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
