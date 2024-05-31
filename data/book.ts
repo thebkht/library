e4fbf52ab45d52c6d189e245d3b64155e45ea25c
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-01 03:53:56 +0900

    feat: Add new books to the data file

---
+++ data/book.ts
@@ -0,0 +1,34 @@
+import { Book } from "@/lib/types/book";
+
+export const books: Book[] = [
+  {
+    id: "1",
+    title: "The Man in the High Castle",
+    author: "Philip K. Dick",
+    cover: "/books/Man In The High Castle.jpg",
+    description: `In this Hugo Award–winning alternative history classic—the basis for the Amazon Original series—the United States lost World War II and was subsequently divided between the Germans in the East and the Japanese in the West.\nIt’s America in 1962. Slavery is legal once again. The few Jews who still survive hide under assumed names. In this world, we meet characters like Frank Frink, a dealer of counterfeit Americana who is himself hiding his Jewish ancestry; Nobusuke Tagomi, the Japanese trade minister in San Francisco, unsure of his standing within the bureaucracy and Japan's with Germany; and Juliana Frink, Frank's ex-wife, who may be more important than she realizes.\nThese seemingly disparate characters gradually realize their connections to each other just as they realize that something is not quite right about their world. And it seems as though the answers might lie with Hawthorne Abendsen, a mysterious and reclusive author, whose best-selling novel describes a world in which the US won the War... The Man in the High Castle is Dick at his best, giving readers a harrowing vision of the world that almost was.\n“The single most resonant and carefully imagined book of Dick’s career.”—New York Times`,
+    publishedDate: "2012-01-24T12:00:00.000Z",
+    purchuaseDate: "2024-02-04T12:00:00.000Z",
+    type: "paperback",
+  },
+  {
+    id: "2",
+    title: "1984",
+    author: "George Orwell",
+    cover: "/books/1984 75th Anniversary.jpg",
+    description: `Written 75 years ago, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever...
+
+This 75th Anniversary Edition includes:
+• A New Introduction by Dolen Perkins-Valdez, author of Take My Hand, winner of the 2023 NAACP Image Award for Outstanding Literary Work—Fiction
+• A New Afterword by Sandra Newman, author of Julia: A Retelling of George Orwell’s 1984
+
+“The Party told you to reject the evidence of your eyes and ears. It was their final, most essential command.”
+
+Winston Smith toes the Party line, rewriting history to satisfy the demands of the Ministry of Truth. With each lie he writes, Winston grows to hate the Party that seeks power for its own sake and persecutes those who dare to commit thoughtcrimes. But as he starts to think for himself, Winston can’t escape the fact that Big Brother is always watching...
+
+A startling and haunting novel, 1984 creates an imaginary world that is completely convincing from start to finish. No one can deny the novel’s hold on the imaginations of whole generations, or the power of its admonitions—a power that seems to grow, not lessen, with the passage of time.`,
+    publishedDate: "1961-01-01T12:00:00.000Z",
+    purchuaseDate: "",
+    type: "paperback",
+  },
+];
