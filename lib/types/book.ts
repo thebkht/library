ba4054c51ff46d095e89244320ecd9fe8acad9b4
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-01 03:52:03 +0900

    feat: Add book type definition

---
+++ lib/types/book.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+const bookSchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  author: z.string(),
+  cover: z.string(),
+  description: z.string(),
+  publishedDate: z.string(),
+  purchuaseDate: z.string(),
+  type: z.enum(["hardcover", "paperback", "ebook", "audiobook"]),
+});
+
+export type Book = z.infer<typeof bookSchema>;
