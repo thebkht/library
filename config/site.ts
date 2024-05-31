b480fc1cef44b000a7c52a73206d6d179818acd6
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-01 03:02:38 +0900

    chore: Update siteConfig links in config/site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
-  name: "bkht / library",
+  name: "bkhtdev/library",
   url: "https://ui.bkhtdev.com",
   ogImage: "https://ui.bkhtdev.com/og.jpg",
   description:
     "A personal library of books, articles, and other resources that I've found useful.",
   links: {
-    twitter: "https://twitter.com/yusupovbg",
-    github: "https://github.com/yusupovbg/library",
+    twitter: "https://twitter.com/thebkht",
+    github: "https://github.com/thebkht/library",
   },
 };
 
