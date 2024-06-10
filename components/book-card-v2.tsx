22c01d073140f50bfdf13e74df789587509fc516
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-11 07:10:56 +0900

    update browse book card

---
+++ components/book-card-v2.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Book } from "@/lib/types/book";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import Image from "next/image";
+import { Skeleton } from "./ui/skeleton";
+import { AspectRatio } from "./ui/aspect-ratio";
+
+export function BookCardV2({ book, ...props }: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
+     return (<>
+          <Card {...props}>
+               <CardContent className="p-0">
+                    <AspectRatio ratio={3 / 4} style={{ position: "relative" }}>
+                         {
+                              book.cover !== "" ? (
+                                   <Image
+                                        src={book.cover}
+                                        alt={book.title}
+                                        layout="fill"
+                                        objectFit="cover"
+                                        objectPosition="center"
+                                   />
+                              ) : null
+                         }
+                         <Skeleton className="h-full w-full" />
+                    </AspectRatio>
+                    <CardHeader className="p-5 border-t border-dashed">
+                         <CardDescription>{book.author}</CardDescription>
+                         <CardTitle className="line-clamp-2 leading-snug">{book.title}</CardTitle>
+                    </CardHeader>
+               </CardContent>
+          </Card>
+     </>)
+}
--- components/book-recommendation.tsx
+++ components/book-recommendation.tsx
@@ -1,20 +1,14 @@
 "use client"
-import {
-     Carousel,
-     CarouselContent,
-     CarouselItem,
-     CarouselNext,
-     CarouselPrevious,
-} from "@/components/ui/carousel"
-import { BookCardV1 } from "@/components/book-card-v1"
+
 import { books } from "@/data/book"
+import { BookCardV2 } from "./book-card-v2"
 
 export function BookRecommendation() {
      return (
           <div className="w-full mt-5 grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 gap-6">
                {
                     books.map((book) => (
-                         <BookCardV1 book={book} className="hover:bg-secondary outline-offset-0 h-full" key={book.id} />
+                         <BookCardV2 book={book} className="hover:bg-secondary outline-offset-0 h-full" key={book.id} />
                     ))
                }
                {/* <Carousel opts={{
