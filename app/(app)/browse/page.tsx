3612ebc48d849fb2aa3f9bcdefa54bc6ddbb3787
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-25 20:27:04 +0900

    Refactor browse page layout for better responsiveness

--- app/(app)/browse/page.tsx
+++ app/(app)/browse/page.tsx
@@ -4,7 +4,7 @@ import { BookRecommendation } from "@/components/book-recommendation"
 export default function BrowsePage() {
 
      return (
-          <div className="container relative px-5 my-10 mt-20 flex-1 flex flex-col">
+          <div className="container relative px-5 my-20 flex-1 flex flex-col">
                <div className="flex justify-center"></div>
                <BookRecommendation />
           </div>
