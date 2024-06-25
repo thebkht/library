9f75cad7f0e548dbee58f525cf0498b1898ef909
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-06-25 20:43:04 +0900

    refactor: Improve book card component by adding link to book details page

--- components/book-card-v1.tsx
+++ components/book-card-v1.tsx
@@ -5,29 +5,34 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
 import { AspectRatio } from "./ui/aspect-ratio";
+import Link from "next/link";
 
 export function BookCardV1({ book, ...props }: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
      return (<>
           <Card {...props}>
                <CardContent className="p-0">
-                    <div className="w-full h-auto">
-                         {
-                              book.cover !== "" ? (
-                                   <Image
-                                        src={book.cover}
-                                        alt={book.title}
-                                        layout="responsive"
-                                        width={1000}
-                                        height={500}
-                                        objectPosition="center"
-                                   />
-                              ) : null
-                         }
-                         <Skeleton className="h-full w-full" />
-                    </div>
+                    <Link href={`/book/${book.slug}`}>
+                         <div className="w-full h-auto">
+                              {
+                                   book.cover !== "" ? (
+                                        <Image
+                                             src={book.cover}
+                                             alt={book.title}
+                                             layout="responsive"
+                                             width={1000}
+                                             height={500}
+                                             objectPosition="center"
+                                        />
+                                   ) : null
+                              }
+                              <Skeleton className="h-full w-full" />
+                         </div>
+                    </Link>
                     <CardHeader className="p-5 border-t border-dashed">
                          <CardDescription className="line-clamp-1">{book.author}</CardDescription>
-                         <CardTitle className="line-clamp-2 leading-snug">{book.title}</CardTitle>
+                         <Link href={`/book/${book.slug}`}>
+                              <CardTitle className="line-clamp-2 leading-snug">{book.title}</CardTitle>
+                         </Link>
                     </CardHeader>
                </CardContent>
           </Card>
