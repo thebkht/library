cda0f710a994155f2c199b8feb28020a6ac47c65
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-10-24 02:01:31 +0900

    refactor: Update site header component

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,36 +1,34 @@
-import Link from "next/link"
+import Link from "next/link";
 
-import { siteConfig } from "@/config/site"
-import { cn } from "@/lib/utils"
-import { CommandMenu } from "@/components/command-menu"
-import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
-import { MobileNav } from "@/components/mobile-nav"
-import { ModeToggle } from "@/components/mode-toggle"
-import { buttonVariants } from "@/components/ui/button"
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { MainNav } from "@/components/main-nav";
+import { MobileNav } from "@/components/mobile-nav";
+import { ModeToggle } from "@/components/mode-toggle";
+import { buttonVariants } from "@/components/ui/button";
 
 export function SiteHeader() {
-     return (
-          <header className="flex-none h-auto left-1/2 fixed top-0 -translate-x-1/2 z-50 w-full bg-background">
-               <div className="flex h-14 w-full justify-center relative items-center border-b border-dashed">
-                    <div className="flex items-center justify-center flex-[1_0_0] gap-2.5 h-full max-w-[1488px] w-[1px] relative">
-                         <div className="flex items-center justify-center flex-[1_0_0] h-full max-w-[1440px] relative w-[1px] pl-6">
-                              <Link className="flex flex-none items-center justify-center relative h-min w-min" href="/">
-                                   <div className="flex-none h-auto w-auto relative">
-                                        <div className="flex justify-center items-center gap-2 relative w-max h-min">
-                                             <Icons.logo className="h-6 w-6" />
-                                             <p className="text-xl font-bold">
-                                                  {siteConfig.name}
-                                             </p>
-                                        </div>
-                                   </div>
-                              </Link>
-                              <div className="flex items-center justify-end flex-[1_0_0] gap-2.5 h-full">
-
-                              </div>
-                         </div>
-                    </div>
-                    {/* <MainNav />
+  return (
+    <header className="flex-none h-auto left-1/2 fixed top-0 -translate-x-1/2 z-50 w-full bg-background">
+      <div className="flex h-14 w-full justify-center relative items-center border-b border-dashed">
+        <div className="flex items-center justify-center flex-[1_0_0] gap-2.5 h-full max-w-[1488px] w-[1px] relative">
+          <div className="flex items-center justify-center flex-[1_0_0] h-full max-w-[1440px] relative w-[1px] pl-6">
+            <Link
+              className="flex flex-none items-center justify-center relative h-min w-min"
+              href="/"
+            >
+              <div className="flex-none h-auto w-auto relative">
+                <div className="flex justify-center items-center gap-2 relative w-max h-min">
+                  <Icons.logo className="h-6 w-6" />
+                  <p className="text-xl font-bold">{siteConfig.name}</p>
+                </div>
+              </div>
+            </Link>
+            <div className="flex items-center justify-end flex-[1_0_0] gap-2.5 h-full"></div>
+          </div>
+        </div>
+        {/* <MainNav />
                     <MobileNav />
                     <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
                          <div className="w-full flex-1 md:w-auto md:flex-none">
@@ -74,7 +72,7 @@ export function SiteHeader() {
                               <ModeToggle />
                          </nav>
                     </div> */}
-               </div>
-          </header>
-     )
+      </div>
+    </header>
+  );
 }
