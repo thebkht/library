8594a0e996c42dbdfa4aacab4698e7a5460aa643
Bakhtiyor Ganijon <b.yusupoff001@gmail.com>
2024-10-24 01:50:49 +0900

    refactor: Adjust image aspect ratio in book card components

--- components/book-card-v1.tsx
+++ components/book-card-v1.tsx
@@ -1,40 +1,53 @@
-"use client"
+"use client";
 
 import { Book } from "@/lib/types/book";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
 import { AspectRatio } from "./ui/aspect-ratio";
 import Link from "next/link";
 
-export function BookCardV1({ book, ...props }: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
-     return (<>
-          <Card {...props}>
-               <CardContent className="p-0">
-                    <Link href={`/book/${book.slug}`}>
-                         <div className="w-full h-auto">
-                              {
-                                   book.cover !== "" ? (
-                                        <Image
-                                             src={book.cover}
-                                             alt={book.title}
-                                             layout="responsive"
-                                             width={1000}
-                                             height={500}
-                                             objectPosition="center"
-                                        />
-                                   ) : null
-                              }
-                              <Skeleton className="h-full w-full" />
-                         </div>
-                    </Link>
-                    <CardHeader className="p-5 border-t border-dashed">
-                         <CardDescription className="line-clamp-1">{book.author}</CardDescription>
-                         <Link href={`/book/${book.slug}`}>
-                              <CardTitle className="line-clamp-2 leading-snug">{book.title}</CardTitle>
-                         </Link>
-                    </CardHeader>
-               </CardContent>
-          </Card>
-     </>)
+export function BookCardV1({
+  book,
+  ...props
+}: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
+  return (
+    <>
+      <Card {...props}>
+        <CardContent className="p-0">
+          <Link href={`/book/${book.slug}`}>
+            <div className="w-full h-auto">
+              {book.cover !== "" ? (
+                <Image
+                  src={book.cover}
+                  alt={book.title}
+                  layout="responsive"
+                  width={1000}
+                  height={500}
+                  objectPosition="center"
+                />
+              ) : null}
+              <Skeleton className="h-full w-full" />
+            </div>
+          </Link>
+          <CardHeader className="p-5 border-t border-dashed sr-only">
+            <CardDescription className="line-clamp-1">
+              {book.author}
+            </CardDescription>
+            <Link href={`/book/${book.slug}`}>
+              <CardTitle className="line-clamp-2 leading-snug">
+                {book.title}
+              </CardTitle>
+            </Link>
+          </CardHeader>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
--- components/book-card-v2.tsx
+++ components/book-card-v2.tsx
@@ -1,39 +1,52 @@
-"use client"
+"use client";
 
 import { Book } from "@/lib/types/book";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import Image from "next/image";
 import { Skeleton } from "./ui/skeleton";
 import { AspectRatio } from "./ui/aspect-ratio";
 import Link from "next/link";
 
-export function BookCardV2({ book, ...props }: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
-     return (<>
-          <Card {...props}>
-               <CardContent className="p-0">
-                    <Link href={`/book/${book.slug}`}>
-                         <AspectRatio ratio={2 / 3} style={{ position: "relative" }}>
-                              {
-                                   book.cover !== "" ? (
-                                        <Image
-                                             src={book.cover}
-                                             alt={book.title}
-                                             layout="fill"
-                                             objectFit="cover"
-                                             objectPosition="center"
-                                        />
-                                   ) : null
-                              }
-                              <Skeleton className="h-full w-full" />
-                         </AspectRatio>
-                    </Link>
-                    <CardHeader className="p-5 border-t border-dashed">
-                         <CardDescription className="line-clamp-1">{book.author}</CardDescription>
-                         <Link href={`/book/${book.slug}`}>
-                              <CardTitle className="line-clamp-2 leading-snug">{book.title}</CardTitle>
-                         </Link>
-                    </CardHeader>
-               </CardContent>
-          </Card>
-     </>)
+export function BookCardV2({
+  book,
+  ...props
+}: { book: Book } & React.ComponentPropsWithoutRef<typeof Card>) {
+  return (
+    <>
+      <Card {...props}>
+        <CardContent className="p-0">
+          <Link href={`/book/${book.slug}`}>
+            <AspectRatio ratio={2 / 3} style={{ position: "relative" }}>
+              {book.cover !== "" ? (
+                <Image
+                  src={book.cover}
+                  alt={book.title}
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition="center"
+                />
+              ) : null}
+              <Skeleton className="h-full w-full" />
+            </AspectRatio>
+          </Link>
+          <CardHeader className="p-5 border-t border-dashed sr-only">
+            <CardDescription className="line-clamp-1">
+              {book.author}
+            </CardDescription>
+            <Link href={`/book/${book.slug}`}>
+              <CardTitle className="line-clamp-2 leading-snug">
+                {book.title}
+              </CardTitle>
+            </Link>
+          </CardHeader>
+        </CardContent>
+      </Card>
+    </>
+  );
 }
